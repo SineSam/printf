2cbde23e67161d3a047c9101fe6e62f37bddf27a
Phumzakose <kosephumza@gmail.com>
2023-05-29 15:53:13 -0700

    added the needed facilities in stdarg library and switch statements

--- _printf.c
+++ _printf.c
@@ -10,5 +10,43 @@
 
 int _printf(const char *format, ...)
 {
+	int i, count = 0;
+	va_list list;
 
+	va_start(list, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					_putchar("%s%c", va_arg(list, format));
+					count++;
+					break;
+				case 's':
+
+					_putchar("%s%s", va_arg(list, format));
+					count++;
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+				count++;
+		}
+	}
+	va_end(list);
+	return (count);
 }
