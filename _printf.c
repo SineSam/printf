52defbb326557f285f9fb26c599877cfe65fab37
Samantha <ssnngcongo@gmail.com>
2023-05-29 14:18:54 -0700

    added the prototype and description to the file

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
