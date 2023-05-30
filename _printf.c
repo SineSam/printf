26cbd09d5a9097b09e28521e8681003fcf8ce4bb
Samantha <ssnngcongo@gmail.com>
2023-05-30 15:59:20 -0700

    reduce code lines

--- _printf.c
+++ _printf.c
@@ -12,87 +12,44 @@
 
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	unsigned int count = 0;
+	unsigned int i, count = 0;
+	char c;
 	va_list list;
 
 	va_start(list, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (*format == '%' && format++)
 		{
-			i++;
-			switch (format[i])
+			if (*format == 'c')
+				c = va_arg(list, int);
+				count += write(1, &c, 1);
+			else if (*format == 's')
+				count += write(1, va_arg(list, char *), strlen(va_arg(list, char *)));
+			else if (*format == 'd' || *format == 'i')
 			{
-				case 'c':
-					{
-					int c;
+				int num = va_arg(list, int), len = 0;
+				char num_str[20];
 
-					c = va_arg(list, int);
-					write(1, &c, 1);
-					count++;
-					break;
-					}
-				case 's':
-					{
-					char *s = va_arg(list, char *);
-
-					size_t len = strlen(s);
-
-					write(1, s, len);
-					count += len;
-					break;
-					}
-				case 'd':
-				case 'i':
-					{
-						int num = va_arg(list, int);
-						int num_abs = (num < 0) ? -num : num;
-						char num_str[20];
-						int len = 0;
-						int i;
-
-						if (num < 0)
-						{
-							write(1, "-", 1);
-							count++;
-						}
-						do {
-							num_str[len++] = '0' + (num_abs % 10);
-							num_abs /= 10;
-						} while (num_abs != 0);
-
-						for (i = len - 1; i >= 0; i--)
-						{
-							write(1, &num_str[i], 1);
-							count++;
-						}
-						break;
-					}
-				case '%':
-					{
-						write(1, "%", 1);
-						count++;
-						break;
-					}
-				default:
-					{
-					write(1, "%", 1);
-					count++;
-					write(1, format, 1);
-					count++;
-					break;
-					}
+				if (num < 0 && (++count))
+					write(1, "-", 1);
+				do {
+					num_str[len++] = '0' + ((num < 0 ? -num : num) % 10);
+				} while (num /= 10);
+				while (len--)
+				write(1, &num_str[len], 1), count += 2;
+				count++;
 			}
+			else if (*format == '%')
+				write(1, "%", 1), count++;
+			else
+				write(1, "%", 1), write(1, format, 1), count += 2;
 		}
 		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
+			write(1, format, 1), count++;
+		format++;
 	}
 	va_end(list);
-
 	return (count);
 }
